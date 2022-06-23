ed8ff42b2af726b65ad8fb8f9ce32ed912d470bf
caio barroncas <developer.barroncasadc@gmail.com>
2022-06-22 20:51:10 -0400

    Initial project

---
+++ src/main/java/com/sha/springbootbookseller/repository/projection/IPurchaseItem.java
@@ -0,0 +1,13 @@
+package com.sha.springbootbookseller.repository.projection;
+
+import java.time.LocalDateTime;
+
+public interface IPurchaseItem {
+
+    String getTitle();
+
+    Double getPrice();
+
+    LocalDateTime getPurchaseTime();
+
+}
